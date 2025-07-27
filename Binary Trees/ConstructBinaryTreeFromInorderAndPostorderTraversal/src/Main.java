2299b5d47d92156b472890a216ebb0bff8e8f028
FrozeyNight <frozeynight@gmail.com>
2025-07-27 17:26:11 +0200

    Updated the code to work on every possible tree within parameters. Made multiple changes on vacation on a different device, hence so many changes in one commit.

--- Binary Trees/ConstructBinaryTreeFromInorderAndPostorderTraversal/src/Main.java
+++ Binary Trees/ConstructBinaryTreeFromInorderAndPostorderTraversal/src/Main.java
@@ -20,138 +20,49 @@ public class Main {
 
     public static TreeNode buildTree(int[] inorder, int[] postorder) {
 
-        // Notes:
-        // 1. The root is the last value of the postorder array
-        // 2. The left child of a given node is the one before it in the inorder array (not for the initial root)
-        // 3. The right child of a given node is the one before it in the postorder array (not for the initial root)
-        // 4. (No sir, we have to handle a node just having the right child, or at least the root) There doesn't seem to be a way to handle a node which has only a right child
-        // 5. If the 4th note is
         /*
-        TODO: 1. Handle a case where a 1 node long binary tree is passed.
-              2. If testing fails, fix the "what if a node only has a right child" problem.
-              3. If that isn't it, fix the leafNodes list (what if root has a right child with a right child).
+        TODO: 1. Find a way to make the code more optimized. (I imagine the main thing slowing it down is the .contains command on the parentNodes list)
 
          */
-        if(inorder.length > 1){
-            TreeNode root = new TreeNode(postorder[postorder.length - 1]);
-
-            //int rightChildIndex = postorder.length - 2;
-
-            int rootIndex = 0;
-            int leftChildHelper = 0;
-            for (int i = 0; i < inorder.length; i++) {
-                if(inorder[i] == root.val){
-                    if(i + 1 < inorder.length) leftChildHelper = inorder[i + 1];
-                    else leftChildHelper = postorder[i - 1];
-                    rootIndex = i;
-                    break;
-                }
-            }
-
-            int leftChildIndex = 0;
-            for (int i = 0; i < postorder.length; i++) {
-                if(postorder[i] == leftChildHelper){
-                    if(i > 0) leftChildIndex = i - 1;
-                    break;
-                }
-            }
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
 
 
-            int inorderLeftChildIndex = 0;
-            for (int i = 0; i < postorder.length; i++) {
-                if(inorder[i] == postorder[leftChildIndex]){
-                    inorderLeftChildIndex = i;
-                    break;
-                }
-            }
-
-            List<Integer> leafNodes = new ArrayList<>();
-
-            if(rootIndex - 1 > 0){
-                for (int i = 0; i < postorder.length; i++) {
-                    if(postorder[i] == inorder[rootIndex - 1]) break;
-                    leafNodes.add(postorder[i]);
-                }
-            }
-            else if(rootIndex - 1 == 0) leafNodes.add(postorder[0]);
-            else leafNodes.add(root.val);
-
-            for (int i = rootIndex; i < postorder.length; i++) {
-                if(postorder[i] == inorder[inorder.length - 2]) break;
-                leafNodes.add(postorder[i]);
-            }
-
-            return createNodes(root, inorder, postorder, inorderLeftChildIndex + 1, postorder.length - 1, leafNodes);
-        }
-
-        /*
-        int inorderRightChildIndex = 0;
-        for (int i = 0; i < postorder.length; i++) {
-            if(inorder[i] == postorder[rightChildIndex]){
-                inorderRightChildIndex = i;
+        int inorderRootIndex = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if(inorder[i] == root.val){
+                inorderRootIndex = i;
                 break;
             }
         }
 
-        int[] leftSideInorder = new int[rootIndex];
-        int[] rightSideInorder = new int[inorder.length - rootIndex - 1];
-        System.arraycopy(inorder, 0, leftSideInorder, 0, rootIndex);
-        for (int i = rootIndex + 1; i < inorder.length; i++) {
-            rightSideInorder[i - rootIndex - 1] = inorder[i];
-        }
+        return createNodes(root, inorder, postorder, inorderRootIndex, postorder.length - 1, postorder[postorder.length - 1], 0, new ArrayList<>());
 
-        int[] leftSidePostorder = new int[leftChildIndex];
-        int[] rightSidePostorder = new int[postorder.length - leftChildIndex - 2];
-        System.arraycopy(inorder, 0, leftSidePostorder, 0, leftChildIndex);
-        for (int i = leftChildIndex + 1; i < postorder.length - 1; i++) {
-            rightSidePostorder[i - leftChildIndex - 1] = postorder[i];
-        }
-
-
-
-
-         */
-
-        //System.out.println(postorder[leftChildIndex]);
-        //System.out.println(postorder[rightChildIndex]);
-
-        return new TreeNode(inorder[0]);
-
-        //return new TreeNode(postorder[postorder.length - 1], createNodes(new TreeNode(postorder[leftChildIndex]), leftSideInorder, leftSidePostorder, inorderLeftChildIndex, leftChildIndex), createNodes(new TreeNode(postorder[postorder.length - 2]), rightSideInorder, rightSidePostorder, inorderRightChildIndex - rootIndex - 1, rightSidePostorder.length - 1));
     }
 
-    private static TreeNode createNodes(TreeNode node, int[] inorder, int[] postorder, int inIndex, int postIndex, List<Integer> leafNodes){
+    private static TreeNode createNodes(TreeNode node, int[] inorder, int[] postorder, int inIndex, int postIndex, int parentValue, int amountOfRightTurns, List<Integer> parentNodes){
         if(node == null) return null;
         else if(inorder.length == 1) return node;
 
-        System.out.println(node.val);
+        parentNodes.add(node.val);
 
-        node.left = new TreeNode(inorder[inIndex - 1]);
-        System.out.println(node.left.val);
-        if(inorder[inIndex - 1] != postorder[postIndex - 1]) node.right = new TreeNode(postorder[postIndex - 1]);
-        if(node.right != null) System.out.println(node.right.val);
 
-        /*
-        int leftChildPostOrderIndex = 0;
-        int rightChildPostOrderIndex = 0;
+        int leftChildPostorderIndex = inIndex - 1 - amountOfRightTurns;
+        int rightChildPostorderIndex = postIndex - 1;
 
-        for (int i = 0; i < postorder.length; i++) {
-            if(postorder[i] == inorder[inIndex - 1]) leftChildPostOrderIndex = i;
-            if(postorder[i] == postorder[postIndex - 1]) rightChildPostOrderIndex = i;
-        }
-         */
+        if(inIndex - 1 >= 0 && inIndex <= inorder.length && inorder[inIndex - 1] != parentValue && !parentNodes.contains(inorder[inIndex - 1])) node.left = new TreeNode(postorder[leftChildPostorderIndex]);
 
-        int leftChildPostOrderIndex = 0;
-        int rightChildInOrderIndex = 0;
+        if(inIndex >= -1 && inIndex + 1 < inorder.length && inorder[inIndex + 1] != parentValue && !parentNodes.contains(inorder[inIndex + 1])) node.right = new TreeNode(postorder[rightChildPostorderIndex]);
 
-        for (int i = 0; i < postorder.length; i++) {
-            if(postorder[i] == inorder[inIndex - 1]) leftChildPostOrderIndex = i;
-            if(inorder[i] == postorder[postIndex - 1]) rightChildInOrderIndex = i;
+        int leftChildInorderIndex = 0;
+        int rightChildInorderIndex = 0;
+
+        for (int i = 0; i < inorder.length; i++) {
+            if(node.left != null && inorder[i] == postorder[leftChildPostorderIndex]) leftChildInorderIndex = i;
+            else if (node.right != null && inorder[i] == postorder[rightChildPostorderIndex]) rightChildInorderIndex = i;
         }
 
-        if(node.left != null && !leafNodes.contains(node.left.val)) createNodes(node.left, inorder, postorder, inIndex - 1, leftChildPostOrderIndex, leafNodes);
-        //createNodes(node.right, inorder, postorder, inIndex + 1, rightChildPostOrderIndex - 1);
-        if(node.right != null && !leafNodes.contains(node.right.val)) createNodes(node.right, inorder, postorder, rightChildInOrderIndex, postIndex - 1, leafNodes);
+        if(node.left != null) createNodes(node.left, inorder, postorder, leftChildInorderIndex, leftChildPostorderIndex, node.val, amountOfRightTurns, parentNodes);
+        if(node.right != null) createNodes(node.right, inorder, postorder, rightChildInorderIndex, rightChildPostorderIndex, node.val, amountOfRightTurns + 1 , parentNodes);
 
         return node;
     }
