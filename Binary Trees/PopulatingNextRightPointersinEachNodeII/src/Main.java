5bfe021e47bf18ec9d79bd12a6b9d2764f00d5f9
FrozeyNight <frozeynight@gmail.com>
2025-08-06 18:16:49 +0200

    Solved the problem by reversing the order of which this method runs from bottom-top to top-bottom. This means all possible connections are made and the correct ones are made last. This does mean that this solution is quite slow, because it has to run through every possible combination.

--- Binary Trees/PopulatingNextRightPointersinEachNodeII/src/Main.java
+++ Binary Trees/PopulatingNextRightPointersinEachNodeII/src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         if(root == null) return null;
 
         if(root.left != null){
-            connect(root.left);
             if(root.right != null){
                 root.left.next = root.right;
                 subtreesConnect(root.left, root.right);
             }
+            connect(root.left);
         }
 
         if(root.right != null) connect(root.right);
@@ -20,23 +20,22 @@ public class Main {
     }
 
     private void subtreesConnect(Node leftNode, Node rightNode){
-        if(leftNode.right != null && rightNode.left != null) {
-            leftNode.right.next = rightNode.left;
-            subtreesConnect(leftNode.right, rightNode.left);
-            if(leftNode.left != null && leftNode.right.left == null && leftNode.right.right == null) subtreesConnect(leftNode.left, rightNode.left);
-        }
-        else if(leftNode.right != null && rightNode.right != null){
-            leftNode.right.next = rightNode.right;
-            subtreesConnect(leftNode.right, rightNode.right);
-            if(leftNode.left != null && leftNode.right.left == null && leftNode.right.right == null) subtreesConnect(leftNode.left, rightNode.right);
-        }
-        else if(leftNode.left != null && rightNode.left != null){
-            leftNode.left.next = rightNode.left;
-            subtreesConnect(leftNode.left, rightNode.left);
-        }
-        else if(leftNode.left != null && rightNode.right != null){
+        if(leftNode.left != null && rightNode.right != null){
             leftNode.left.next = rightNode.right;
             subtreesConnect(leftNode.left, rightNode.right);
         }
+        if(leftNode.left != null && rightNode.left != null){
+            leftNode.left.next = rightNode.left;
+            subtreesConnect(leftNode.left, rightNode.left);
+        }
+        if(leftNode.right != null && rightNode.right != null){
+            leftNode.right.next = rightNode.right;
+            subtreesConnect(leftNode.right, rightNode.right);
+        }
+        if(leftNode.right != null && rightNode.left != null) {
+            leftNode.right.next = rightNode.left;
+            subtreesConnect(leftNode.right, rightNode.left);
+        }
+
     }
 }
