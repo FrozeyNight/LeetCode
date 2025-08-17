904f51d6cc72d2115a16748bed00a1641458fc3f
FrozeyNight <frozeynight@gmail.com>
2025-08-17 19:49:14 +0200

    Finished my own solution, which works really well, but uses a global variable and provides a value of the LCA instead of a reference to the node in the tree that is LCA. Therefore, I also implemented a cleaner solution from ChatGPT.

--- Binary Trees/LowestCommonAncestorofaBinaryTree/src/Main.java
+++ Binary Trees/LowestCommonAncestorofaBinaryTree/src/Main.java
@@ -3,30 +3,44 @@ public class Main {
 
     }
 
-    // First method simply looks through a tree to find one of the two values. (Probably use DFS) <- (yup probably inorder traversal)
-    // Once one of them is found, it looks if the other node is a far child of it, and if yes, then it returns itself as the answer.
-    // If not, we mark the parent of that node as the LCA and check if the other target node is in the right subtree.
-    // If yes, we return the marked node as the LCA
-    // If not, we mark its parent as the LCA and repeat this process until we find the answer.
-
-    //TODO: Make it work if the second target node is a far child of the other target node
-
-    public int LCA = 0;
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-          findLCA(root, p.val, q.val);
-          int answer = LCA;
-          LCA = 0;
-          return new TreeNode(answer);
-    }
+        if(root == null || root == p || root == q) return root;
 
-    public boolean findLCA(TreeNode node, int p, int q){
-        if(node == null) return false;
-        if(findLCA(node.left, p, q) && findLCA(node.right, p, q)) {
-            LCA = node.val;
-            return false;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null){
+            return root;
         }
 
-        return node.val == p || node.val == q;
+        return left != null ? left : right;
     }
+
+    /*
+    private int LCA = 0;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        findLCA(root, p.val, q.val);
+        int answer = LCA;
+        LCA = 0;
+        return new TreeNode(answer);
+    }
+
+    private boolean findLCA(TreeNode node, int p, int q){
+        if(node == null || LCA != 0) return false;
+
+        boolean left = findLCA(node.left, p, q);
+        boolean right = findLCA(node.right, p, q);
+
+        if(left && right || node.val == p || node.val == q){
+            if(left || right){
+                LCA = node.val;
+                return false;
+            }
+        }
+
+        return node.val == p || node.val == q || left || right;
+    }
+
+     */
 }
