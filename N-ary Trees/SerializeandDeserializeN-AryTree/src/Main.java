64ca35b54f5c74c8f2d127d0f84d3c0d49e854d0
FrozeyNight <frozeynight@gmail.com>
2025-09-01 18:59:24 +0200

    Set up the project for serializing and deserializing N-ary tree.

---
+++ N-ary Trees/SerializeandDeserializeN-AryTree/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+    public String serialize(Node root) {
+
+    }
+
+    public Node deserialize(String data) {
+
+    }
+}
