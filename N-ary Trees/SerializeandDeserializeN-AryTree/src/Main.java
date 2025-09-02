328324622f9cb2159f2ac41b48d5a836971b5c34
FrozeyNight <frozeynight@gmail.com>
2025-09-02 21:11:08 +0200

    In progress of adding serialization.

--- N-ary Trees/SerializeandDeserializeN-AryTree/src/Main.java
+++ N-ary Trees/SerializeandDeserializeN-AryTree/src/Main.java
@@ -1,10 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
 
     }
 
     public String serialize(Node root) {
+        List<Integer> preorder = new ArrayList<>();
+        if(root == null) return null;
+        preorder.add(root.val);
+        traverse(preorder, root);
 
+        StringBuilder serializePreorder = new StringBuilder();
+
+        for (Integer node : preorder){
+            serializePreorder.append(node).append(" ");
+        }
+
+        return String.valueOf(serializePreorder);
+    }
+
+    private void traverse(List<Integer> preorder, Node node){
+        if(node.children.isEmpty()) return;
+
+        int amountOfChildren = node.children.size();
+
+        for (int i = 0; i < amountOfChildren; i++) {
+            preorder.add(node.children.get(i).val);
+            traverse(preorder, node.children.get(i));
+            if(i + 1 != amountOfChildren) preorder.add(100000);
+            else preorder.add(null);
+        }
     }
 
     public Node deserialize(String data) {
