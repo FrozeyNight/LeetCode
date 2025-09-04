4c57af4c97a760fa915a0e2e48d272fe89eb38e0
FrozeyNight <frozeynight@gmail.com>
2025-09-04 21:06:35 +0200

    Finished and improved the serialize method by changing the way we serialize the N-ary tree to only work off of numbers and nulls instead of relying on numbers outside of exercise constraints.

--- N-ary Trees/SerializeandDeserializeN-AryTree/src/Main.java
+++ N-ary Trees/SerializeandDeserializeN-AryTree/src/Main.java
@@ -9,37 +9,65 @@ public class Main {
         Node root = new Node(tree[0], new ArrayList<>());
         testTreeBuilder(tree, root);
         System.out.println(serialize(root));
+        deserialize(serialize(root));
     }
 
     public static String serialize(Node root) {
         List<Integer> preorder = new ArrayList<>();
         if(root == null) return null;
         preorder.add(root.val);
-        traverse(preorder, root);
+
+        if(root.children.isEmpty()){
+            preorder.add(null);
+        }
+
+        int amountOfChildren = root.children.size();
+
+        for (int i = 0; i < amountOfChildren; i++) {
+            preorder.add(root.children.get(i).val);
+            traverse(preorder, root.children.get(i));
+            preorder.add(null);
+        }
+
+        if(preorder.size() > 2) preorder = preorder.subList(0, preorder.size() - 2);
 
         StringBuilder serializePreorder = new StringBuilder();
 
         for (Integer node : preorder){
-            serializePreorder.append(node).append(" ");
+            serializePreorder.append(node).append(",");
         }
+
+        int stringLen = serializePreorder.length();
+        if(stringLen > 1) serializePreorder.deleteCharAt(stringLen - 1);
 
         return String.valueOf(serializePreorder);
     }
 
     private static void traverse(List<Integer> preorder, Node node){
-        if(node.children.isEmpty()) return;
+        if(node.children.isEmpty()){
+            preorder.add(null);
+            return;
+        }
 
         int amountOfChildren = node.children.size();
 
         for (int i = 0; i < amountOfChildren; i++) {
             preorder.add(node.children.get(i).val);
             traverse(preorder, node.children.get(i));
-            if(i + 1 != amountOfChildren) preorder.add(100000);
-            else preorder.add(null);
         }
     }
 
-    public Node deserialize(String data) {
+    public static Node deserialize(String data) {
+        if(data == null) return null;
+
+        String[] preorderInString = data.split(",");
+        int arrLen = preorderInString.length;
+        Integer[] preorder = new Integer[arrLen];
+        for (int i = 0; i < arrLen; i++) {
+            if(preorderInString[i].equals("null")) continue;
+            preorder[i] = Integer.parseInt(preorderInString[i]);
+        }
+
         return null;
     }
 
